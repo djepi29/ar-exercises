2770489ea11593b51aa48ccc319db2092e29edeb
djepi29 <j3anpi3rr3g3org3s@email.com>
2023-07-01 15:43:45 -0400

    custom store  validation

--- lib/store.rb
+++ lib/store.rb
@@ -3,4 +3,11 @@ has_many :employees
 
 validates :name, presence: true, length: { minimum: 3 }
 validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+validate :must_carry_apparel
+
+def must_carry_apparel
+  if !mens_apparel && !womens_apparel
+    errors.add(:base, "Store must carry at least one type of apparel")
+  end
+end
 end
